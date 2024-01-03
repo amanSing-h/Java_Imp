45073d149f479e34f714010f04243905f1a50d92
Aman <amansingh23181@gmail.com>
2024-01-03 12:40:21 +0530

    2
    2nd

---
+++ The10_8ConstraintRuleForTimeComplexity.java
@@ -0,0 +1,16 @@
+public class The10_8ConstraintRuleForTimeComplexity {
+
+    /*
+    10^8 constraint rule for time complexity
+if value is :
+<= [10......11]    ->  O(n!), O(n^6)
+<= [15......18]    -> O(2^n * n^2)
+< 100                  -> O(n^4)
+<400                   -> O(n^3)
+<2000                 -> O(n^2 & logn)
+10^4                    -> O(n^2)
+10^6                    -> O(nlogn)
+10^8                    -> O(n), O(logn)
+     */
+
+}
